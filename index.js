d876fc52dbb2e62272e118da08f3f5e56173948f
TabithaKoech <tabbyk1718@gmail.com>
2022-06-12 18:55:08 +0300

    commit tree b7a0833322d4a79dd6113d80026e26558af32b09

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Code your solution here
+const drivers = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+
+  function findMatching(drivers,name){
+    return drivers.filter(function(driverNames)
+    {
+        return driverNames.toLowerCase() === name.toLowerCase();
+    })
+
+  }
+
+  function fuzzyMatch(array, string) {
+    return array.filter(function(element) {
+        return element.startsWith(string);
+    });
+}
+
+function matchName(array, name) {
+    return array.filter(function(driver) {
+        return driver.name === name;
+    });
+}
